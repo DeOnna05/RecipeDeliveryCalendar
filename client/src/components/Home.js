627838cd012e15d125d5598ff26855fc9bf8de18
DeOnna <36292560+DeOnna05@users.noreply.github.com>
2018-11-23 23:33:48 -0600

    updates

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,41 +2,43 @@ import React from 'react';
 import { Container, Row, Col, Card, CardImg, CardText, CardBody, CardLink, CardTitle, CardSubtitle, UncontrolledCarousel, Button } from 'reactstrap';
 // import logo from '../public/media/RecipeDeliveryLogo.png'
 const items = [
-  {
-    src: 'https://www.freedombykevin.com/wp-content/uploads/2017/09/Cooking-Techniques_Hero.jpg',
-    altText: 'Slide 1',
-    caption: 'Save time on meal planning. Recipe Calendar plans all of your meals for the coming days and weeks.',
-    header: 'Save Time'
-  },
-  {
-    src: 'https://siaaustria.com/wp-content/uploads/2018/03/meals.jpg',
-    altText: 'Slide 2',
-    caption: 'Save the recipes you love so your never left feeling uninspired. Your favorite recipes are right at your fingertips.',
-    header: 'Save Recipes'
-  },
-  {
-    src: 'https://8bnztmont6-flywheel.netdna-ssl.com/wp-content/uploads/2017/10/833018_JbEB3pOw.jpg',
-    altText: 'Slide 3',
-    caption: 'Save money by avoiding waste because you only by the groceries you need. Planning ahead also saves money spent on eating out.',
-    header: 'Save Money'
-  }
+    {
+        src: 'https://www.freedombykevin.com/wp-content/uploads/2017/09/Cooking-Techniques_Hero.jpg',
+        altText: 'Slide 1',
+        caption: 'Save time on meal planning. Recipe Calendar plans all of your meals for the coming days and weeks.',
+        header: 'Save Time'
+    },
+    {
+        src: 'https://siaaustria.com/wp-content/uploads/2018/03/meals.jpg',
+        altText: 'Slide 2',
+        caption: 'Save the recipes you love so your never left feeling uninspired. Your favorite recipes are right at your fingertips.',
+        header: 'Save Recipes'
+    },
+    {
+        src: 'https://8bnztmont6-flywheel.netdna-ssl.com/wp-content/uploads/2017/10/833018_JbEB3pOw.jpg',
+        altText: 'Slide 3',
+        caption: 'Save money by avoiding waste because you only by the groceries you need. Planning ahead also saves money spent on eating out.',
+        header: 'Save Money'
+    }
 ];
 
 const Home = () => {
-    return(
+    return (
         <Container>
             <Card>
                 <CardBody>
                     <CardTitle><img width="100%" src="" alt="Logo" /></CardTitle>
-                    <CardSubtitle>Card subtitle</CardSubtitle>
                     <UncontrolledCarousel items={items} />
-                </CardBody>     
-                <CardBody>
-                
+                    <CardBody>                        
+                    </CardBody>
+                    <Button color="info" size="lg" block>Sign Up</Button>
+                    <Button outline color="info" size="lg" block>Sign In</Button>
                 </CardBody>
-                <Button color="primary" size="lg" block>Block level button</Button>
-<Button color="secondary" size="lg" block>Block level button</Button>
-            </Card>     
+                <CardBody>
+
+                </CardBody>
+
+            </Card>
         </Container>
     )
 }
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import './Login.css';
+import { InputGroup, InputGroupAddon, InputGroupText, Input, Container, Row, Col, Card, CardImg, CardText, CardBody, CardLink, CardTitle, CardSubtitle, UncontrolledCarousel, Button, Carousel } from 'reactstrap';
 
 
 
@@ -11,58 +12,70 @@ export default class Login extends React.Component {
         error: ""
     }
 
-handleChange = (event) => {
-        this.setState({[event.target.name]: event.target.value})
+    handleChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value })
     }
 
-submitClick = (event) => {
-    event.preventDefault();
-    console.log(this.state.username, "username");
-    console.log(this.state.password, "password");
-    axios.post('/api/login', {
-        username: this.state.username,
-        password:this.state.password
-    }).then(res => {
-        console.log(res, "response from login");
-    });
-}
-  
+    submitClick = (event) => {
+        event.preventDefault();
+        console.log(this.state.username, "username");
+        console.log(this.state.password, "password");
+        axios.post('/api/login', {
+            username: this.state.username,
+            password: this.state.password
+        }).then(res => {
+            console.log(res, "response from login");
+        });
+    }
+
     render() {
-        const {error} = this.state;
+        const { error } = this.state;
         return (
-        
-            <div className="center">
-                <div className="card">
-              
-                    <h1>Login</h1>
-                    <form>
-                        <input
-                            className="username"
-                            placeholder="Email"
-                            id="username"
-                            name="username"
-                            type="text"
-                            value={this.state.username}
-                            onChange={event=>this.handleChange(event)}
-                        />
-                        <input
-                            className="password"
-                            placeholder="Password"
-                            name="password"
-                            type="password"
-                            value={this.state.password}
-                            onChange={event=>this.handleChange(event)}
-                        />
-                        <input
-                            className="submit"
-                            value="SUBMIT"
-                            type="submit"
-                            onClick={event=>this.submitClick(event)}
-                        />
-                    </form>
-                    {error && <p>Invaid Username or Password</p>}
-                </div>
-            </div>
+
+            <Container>
+                <Card>
+                    <CardBody>
+                        <h1>Sign In</h1>
+                        {/* <Carousel.Item>
+                            <img width={900} height={500} alt="900x500" src="/carousel.png" />
+                            <Carousel.Caption>
+                                <h3>First slide label</h3>
+                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item> */}
+                        <InputGroup>
+                            <InputGroupAddon addonType="prepend">@</InputGroupAddon>
+                            <Input className="username"
+                                placeholder="Email"
+                                id="username"
+                                name="username"
+                                type="text"
+                                value={this.state.username}
+                                onChange={event => this.handleChange(event)} />
+                        </InputGroup>
+
+                        <InputGroup>
+                            <Input
+                                className="password"
+                                placeholder="Password"
+                                name="password"
+                                type="password"
+                                value={this.state.password}
+                                onChange={event => this.handleChange(event)} />
+
+                            <input
+                                className="submit"
+                                value="SUBMIT"
+                                type="submit"
+                                onClick={event => this.submitClick(event)}
+                            />
+
+                            {error && <p>Invaid Username or Password</p>}
+                        </InputGroup>
+                    </CardBody>
+
+                </Card>
+            </Container>
         );
     }
 }
