d484c774160a2bb481ebdddb0ec3c741b3a23c45
DeOnna <36292560+DeOnna05@users.noreply.github.com>
2018-11-30 23:02:08 -0600

    update

--- client/src/components/MyNavbar.js
+++ client/src/components/MyNavbar.js
@@ -2,18 +2,22 @@ import React from 'react';
 import axios from 'axios';
 import Scrollspy from './Scrollspy'
 import './MyNavbar.css'
+import Recipes from './Recipes'
+import { Container } from 'reactstrap'
 
 const logout = (event) => {
-    event.preventDefault();
-    axios.get('/api/logout').then(res =>{       
-      localStorage.removeItem('token');
-      console.log(res);
-       window.location = '/';})
+  event.preventDefault();
+  axios.get('/api/logout').then(res => {
+    localStorage.removeItem('token');
+    console.log(res);
+    window.location = '/';
+  })
 }
 
 const MyNavbar = (props) => {
 
   return (
+
     <div>
       <ul className="nav nav-tabs nav-fill" id="myTab" role="tablist">
         <li className="nav-item">
@@ -26,22 +30,29 @@ const MyNavbar = (props) => {
           <a className="nav-link" id="listTab" data-toggle="tab" href="/List" role="tab">Shopping List</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" id="logoutTab" data-toggle="tab" href="/" role="tab" onClick ={ (event) => logout(event)}>Logout</a>
+          <a className="nav-link" id="logoutTab" data-toggle="tab" href="/" role="tab" onClick={(event) => logout(event)}>Logout</a>
         </li>
         <li className="nav-item">
           <a className="navbar-brand" id="logout" href="/"><img className="navLogo" src="/media/RecipeDeliveryLogo.png" alt="Logo" ></img></a>
         </li>
       </ul>
 
-{/* test area for tabs */}
+      {/* test area for tabs */}
       <div className="tab-content" id="myTabContent">
         <div className="tab-pane fade show active" id="menu" role="tabpanel">TEST</div>
         <div className="tab-pane fade" id="queue" role="tabpanel">...</div>
         <div className="tab-pane fade" id="list" role="tabpanel">...</div>
         <div className="tab-pane fade" id="logout" role="tabpanel">...</div>
       </div>
+
+      <Container>
       <Scrollspy/>
+      <br></br>
+      <Recipes/>
+    </Container>
     </div>
+
+    
   )
 }
 
--- client/src/components/Recipes.js
+++ client/src/components/Recipes.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import { Col, Row } from 'reactstrap';
+import './Recipes.css'
 
 
 const Recipes = (props) => {
 
-    <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src=".../100px180/" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+
+    return (
+        <Row>
+            <Col s="12" m="6" lg="4">
+                <div class="card-deck">
+                    <div className="card large hoverable recipeCard">
+                        <div className="card-header">Breakfast</div>
+                        <img className="card-img-top" src="https://greatist.com/sites/default/files/39HealthyBreakfastSwaps_Egg.jpg" alt="Recipe Image"></img>
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" className="btn btn-danger cardBtn">Delete</a>
+                            <a href="#" className="btn btn-info cardBtn">Cook</a>
+                            <a href="#" className="btn btn-info cardBtn">Change</a>
+                        </div>
+                    </div>
+                </div>
+            </Col>
+        </Row>
+    )
 }
+
+export default Recipes
